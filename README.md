a2e69b4a8ba2e5203385b3966a0ab28fa6b33bfb
Aaron Ladage <aladage@degdigital.com>
2020-02-18 22:13:31 -0600

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# storybook-addon-taffy
-A responsive viewport addon for Storybook
+# Taffy
+Taffy is a responsive viewport addon for Storybook. This addon was heavily inspired by [Ish](https://github.com/bradfrost/ish.), the delightful resizer built into [Pattern Lab](https://patternlab.io/), Brad Frost's atomic design visualizer and static site generator.
+
+Although there are other Storybook resizers out there, Taffy brings some great new features to the party, including:
+
+* Dynamic resizing via a draggable handle on the right side of the viewport
+* Precise resizing by value entry, in either pixels or ems
+* One-click small, medium and large breakpoint buttons that resize the viewport to a randomized width within each range. 
+* Full-screen view.
+* Support for resizing withing the Storybook Docs tab
+
+## Installation
+Install the following npm module in your project:
+
+```
+npm i --save-dev @degjs/storybook-addon-taffy
+```
+
+Then, register the addon in the `addons` array in `.storybook/main.js`:
+
+```
+module.exports = {
+    ...
+    addons: [
+        '@degjs/storybook-addon-taffy/register'
+    ]
+};
+```
+
+Finally, import `withTaffy` and add the decorator within `.storybook/preview.js`, like this:
+
+```
+import { addDecorator } from '@storybook/react';
+import { withTaffy } from '@degjs/storybook-addon-taffy';
+
+addDecorator(withTaffy);
+```
+
+That's it! Restart your Storybook build, and you should see Taffy controls in the toolbar, as well as a resize handle on the right side of your story previews.
---
+++ src/register.js
@@ -0,0 +1,166 @@
+import React, { useState } from 'react';
+import { addons } from '@storybook/addons';
+import { useChannel } from '@storybook/api';
+// import { styled, Global, Theme, withTheme } from '@storybook/theming';
+import { Button, Separator } from '@storybook/components';
+import { getArrayItemByValue, getEmFromPx, getPxFromEm, getRandomVal } from './utils';
+import constants from './constants';
+
+const {
+  addonId,
+  panelId,
+  viewModes,
+  presets,
+  wrapperWidthChangeEvent,
+  wrapperMaxWidthChangeEvent,
+  toolWidthChangeEvent
+} = constants;
+
+const TaffyTool = () => {
+  const [toolPxWidth, setToolPxWidth] = useState('');
+  const [maxWidth, setmaxWidth] = useState(0);
+  const [presetWidths, setPresetWidths] = useState(presets);
+
+  const handleWrapperWidthChange = width => {
+    setToolPxWidthAsString(width);
+  };
+
+  const handleMaxWidthChange = width => {
+    const largePreset = getArrayItemByValue(presetWidths, 'large');
+    if (largePreset) {
+      const modifiedLargePreset = {
+        ...largePreset,
+        max: width
+      };
+      const newPresetWidths = presetWidths.map(obj => modifiedLargePreset.id === obj.id ? modifiedLargePreset : obj);
+      setPresetWidths(newPresetWidths);
+      setmaxWidth(width);
+    }
+  };
+
+  const handleInputChange = width => {
+    setToolPxWidth(width);
+  };
+
+  const handleInputBlur = width => {
+    setToolPxWidth(width);
+    emit(toolWidthChangeEvent, width);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setToolPxWidthAsString(toolPxWidth);
+    emit(toolWidthChangeEvent, toolPxWidth);
+  };
+
+  const handlePresetClick = presetId => {
+    const preset = getArrayItemByValue(presetWidths, presetId);
+    const randomWidthWithinRange = getRandomVal(preset.min, preset.max);
+    setToolPxWidthAsString(randomWidthWithinRange);
+    emit(toolWidthChangeEvent, randomWidthWithinRange);
+  };
+
+  const handleFullClick = () => {
+    setToolPxWidthAsString(maxWidth);
+    emit(toolWidthChangeEvent, maxWidth);
+  };
+
+  const emit = useChannel({
+    [wrapperWidthChangeEvent]: handleWrapperWidthChange,
+    [wrapperMaxWidthChangeEvent]: handleMaxWidthChange
+  });
+
+  const setToolPxWidthAsString = width => setToolPxWidth(width.toString());
+
+  return (
+    <>
+      <Separator />
+      <form onSubmit={handleSubmit} style={{
+        alignItems: 'center',
+        display: 'flex'
+      }}>
+        <div className="taffy-field">
+          <input
+            id="px-input"
+            onChange={e => handleInputChange(e.target.value)}
+            onBlur={e => handleInputBlur(e.target.value)}
+            value={toolPxWidth}
+            autoComplete="off"
+            maxLength="5"
+            style={{
+              fontSize: '0.75rem',
+              height: '1.375rem',
+              width: '3.125rem'
+            }}
+          />
+          <label htmlFor="px-input" style={{
+            fontSize: '12px'
+          }}>PX</label>
+        </div>
+        <div className="taffy-field" style={{
+          marginLeft: '0.75rem'
+        }}>
+          <input
+            id="em-input"
+            onChange={e => handleInputChange(getPxFromEm(e.target.value))}
+            onBlur={e => handleInputBlur(getPxFromEm(e.target.value))}
+            value={getEmFromPx(toolPxWidth)}
+            autoComplete="off"
+            maxLength="5"
+            style={{
+              fontSize: '0.75rem',
+              height: '1.375rem',
+              width: '3.125rem'
+            }}
+          />
+          <label htmlFor="em-input" style={{
+            fontSize: '12px'
+          }}>EM</label>
+        </div>
+        <button type="submit" style={{
+          clip: 'rect(1px, 1px, 1px, 1px)',
+          height: '1px',
+          overflow: 'hidden',
+          position: 'absolute',
+          whiteSpace: 'nowrap',
+          width: '1px'
+        }}>Submit</button>
+      </form>
+
+
+      {presetWidths.map(({ id, title, label }) => (
+        <Button
+          key={id}
+          title={title}
+          onClick={() => handlePresetClick(id)}
+          type="button"
+          style={{
+            color: '#999',
+            paddingLeft: '15px',
+            paddingRight: 0
+          }}
+        >{label}</Button>
+      ))}
+      <Button
+        onClick={handleFullClick}
+        title="Full"
+        type="button"
+        style={{
+          color: '#999',
+          paddingLeft: '15px',
+          paddingRight: 0
+        }}
+      >Full</Button>
+    </>
+  );
+};
+
+const addChannel = api => {
+  addons.add(panelId, {
+    ...constants,
+    match: ({ viewMode }) => viewModes.includes(viewMode),
+    render: () => <TaffyTool />
+  });
+};
+
+addons.register(addonId, addChannel);
